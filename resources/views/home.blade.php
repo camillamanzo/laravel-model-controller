6e0b50d8f6971aab6392d3560a219481437049ed
camillamanzo <camillasouthafrica@gmail.com>
2021-11-04 16:15:41 +0100

    added css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,17 +2,18 @@
 
 @section('content')
     <section class="container">
-        @forelse ($movies as $movie)
-            <div class="card">
+        <div class="row">
+            @forelse ($movies as $movie)
+            <div class="card col-5 mx-auto">
                 <h2>Title: "{{ $movie->title }}"</h2>
                 <h3>Original Title: "{{ $movie->original_title }}"</h3>
                 <h3>Nationality: {{ $movie->nationality }}</h3>
                 <h3>Date: {{ $movie->date }}</h3>
                 <h3>Vote: {{ $movie->vote }}</h3>
             </div>
-            <hr>
         @empty
             <h3>There are no available movies.</h3>
         @endforelse
+        </div>
     </section>
 @endsection
